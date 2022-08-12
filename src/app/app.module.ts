4a0e8740f90ae4f4949415d03e4d76554b75b356
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-08-11 20:32:46 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from './modules/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent, HeaderComponent],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      MaterialModule,
+      BrowserAnimationsModule,
+   ],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
